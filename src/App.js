e859186b8ded886ac3ab71b5ee5f6e5689ca77ad
Pete Masse <pfmasse60@gmail.com>
2023-09-05 10:42:33 -0600

    Test commit for new job

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import React, {useState, useRef} from 'react'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  const [startTime, setStartTime] = useState(null)
+  const [now, setNow] = useState(null)
+  const internalRef = useRef(null)
 
-export default App;
+  const handleStart = () => {
+    setStartTime(Date.now())
+    setNow(Date.now())
+
+    clearInterval(internalRef.current)
+    internalRef.current = setInterval(() => {
+      setNow(Date.now())
+    }, 10)
+  }
+
+  const handleStop = () => {
+    clearInterval(internalRef.current)
+  }
+
+  let secondsPassed = 0
+  if (startTime != null && now != null) {
+    secondsPassed = (now - startTime) / 1000
+  }
+
+  return (
+    <>
+      <h1>Time elapsed: {
+        secondsPassed.toFixed(3)
+      }</h1>
+      <button onClick={handleStart}>Start</button>
+      <button onClick={handleStop}>Stop</button>
+    </>
+  )
+}
+export default App
